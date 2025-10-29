e55369f7dcbda7a040995b648812f15adc18d18d
Bill Hughes <whughesiii@pm.me>
2025-10-29 07:54:54 -0500

    new project, what I need?

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm ~/.azure
+
+export XDG_CONFIG_HOME="$HOME"/.config
+mkdir -p "$XDG_CONFIG_HOME"
+
+ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+
+packages=(
+  lazygit 
+  tree-sitter-cli 
+  fzf 
+  fd
+)
+
+sudo apt install npm ripgrep
+
+for pkg in "${packages[@]}"; do
+  npm install "$pkg"
+done
