3b1be6448e87c13c6abf8ff0224e62999f584f7f
Bill Hughes <whughesiii@pm.me>
2025-11-12 10:34:41 -0600

    fix cp command

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ rm ~/.azure
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
-cp "$PWD/dotfiles/nvim" "$XDG_CONFIG_HOME"/nvim
-cp "$PWD/dotfiles/.tmux.conf" "$HOME"/.tmux.conf
+cp -r "$PWD/dotfiles/nvim" "$XDG_CONFIG_HOME"/nvim
+cp -r "$PWD/dotfiles/.tmux.conf" "$HOME"/.tmux.conf
