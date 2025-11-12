dd47bd256f0be957f370a7dff8fb4b3e1f7ac758
Bill Hughes <whughesiii@pm.me>
2025-11-12 10:33:23 -0600

    update to test in new devcontainer

--- setup.sh
+++ setup.sh
@@ -5,19 +5,5 @@ rm ~/.azure
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
-
-packages=(
-  lazygit 
-  tree-sitter-cli 
-  fzf 
-  fd
-)
-
-sudo apt update
-sudo apt install npm ripgrep
-
-for pkg in "${packages[@]}"; do
-  npm install "$pkg"
-done
+cp "$PWD/dotfiles/nvim" "$XDG_CONFIG_HOME"/nvim
+cp "$PWD/dotfiles/.tmux.conf" "$HOME"/.tmux.conf
