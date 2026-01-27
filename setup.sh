572a5a7f7ad190d2a98b9aa5f2ca1963c1d0cfdb
whughesiii2187 <whughesiii@pm.me>
2026-01-26 18:48:04 -0600

    Modify setup.sh for user-specific configuration
    Updated setup script to configure Neovim and tmux for a specific user.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-rm ~/.azure
+VSCODE_USER="vscode"
+VSCODE_HOME="/home/$VSCODE_USER"
 
-export XDG_CONFIG_HOME="$HOME"/.config
+export XDG_CONFIG_HOME="$VSCODE_HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
+apt update 
+apt upgrade
+
+add-apt-repository ppa:neovim-ppa/unstable
+
+apt install neovim
+
 cp -r "$PWD/dotfiles/nvim" "$XDG_CONFIG_HOME"/nvim
-cp -r "$PWD/dotfiles/.tmux.conf" "$HOME"/.tmux.conf
+cp -r "$PWD/dotfiles/.tmux.conf" "$VSCODE_HOME"/.tmux.conf
+cp -r "$PWD/dotfiles/.tmux.conf" "$VSCODE_HOME"/.tmux"
+
+chown -R "$VSCODE_USER:$VSCODE_USER" "$VSCODE_HOME/.config" "$VSCODE_HOME/.tmux*"
