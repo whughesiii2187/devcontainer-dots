d5569951160d4572743860790fbeb22722e006e8
William Hughes <whughesiii@pm.me>
2026-01-27 16:48:57 -0600

    last minute change

--- setup.sh
+++ setup.sh
@@ -6,11 +6,8 @@ VSCODE_HOME="/home/$VSCODE_USER"
 export XDG_CONFIG_HOME="$VSCODE_HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
-sudo apt update -y
-sudo apt upgrade -y
-
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
-
+sudo apt update -y
 sudo apt install -y neovim tmux fzf ripgrep
 
 sudo cp -r "$PWD/dotfiles/nvim" "$XDG_CONFIG_HOME"/nvim
@@ -18,7 +15,6 @@ sudo cp -r "$PWD/dotfiles/.tmux.conf" "$VSCODE_HOME"/.tmux.conf
 sudo cp -r "$PWD/dotfiles/.tmux" "$VSCODE_HOME"/.tmux
 
 sudo chown -R "$VSCODE_USER:$VSCODE_USER" "$VSCODE_HOME"
-
 sudo usermod -s /usr/bin/zsh "$VSCODE_USER"
 
 cat <<'EOF' | sudo -u "$VSCODE_USER" tee -a "$VSCODE_HOME/.zshrc" > /dev/null
