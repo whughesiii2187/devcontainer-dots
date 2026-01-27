4d4d3d29738dded7729e45b565b49cad620581c2
William Hughes <whughesiii@pm.me>
2026-01-27 14:01:56 -0600

    add usermod command for setting shell in devcontainer.

--- setup.sh
+++ setup.sh
@@ -20,3 +20,4 @@ cp -r "$PWD/dotfiles/.tmux" "$VSCODE_HOME"/.tmux
 chown -R "$VSCODE_USER:$VSCODE_USER" "$VSCODE_HOME"
 
 echo alias ff='nvim "$(fzf)"' >> "VSCODE_HOME"/.zshrc
+usermod -s /usr/bin/zsh "$VSCODE_USER"
