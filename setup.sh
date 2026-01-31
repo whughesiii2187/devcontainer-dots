6aeeb7d9b2155b24f787ae60c57bdc1907a66e0e
William Hughes <whughesiii@pm.me>
2026-01-30 22:12:36 -0600

    Change how neovim is installed so we get at least v0.11.4

--- setup.sh
+++ setup.sh
@@ -2,13 +2,30 @@
 
 VSCODE_USER="vscode"
 VSCODE_HOME="/home/$VSCODE_USER"
+NVIMVER=0.11.4
+ARCHTYPE=$(uname -m)
+if [[ "$ARCHTYPE" == "x86_64" ]]; then
+    ARCHTYPE="x86_64"
+elif [[ "$ARCHTYPE" == "aarch64" ]]; then
+    ARCHTYPE="arm64"
 
 export XDG_CONFIG_HOME="$VSCODE_HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
+# sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update -y
-sudo apt install -y neovim tmux fzf ripgrep
+# sudo apt install -y neovim tmux fzf ripgrep
+sudo apt install -y tmux fzf ripgrep file ninja-build gettext cmake unzip curl
+
+cd /tmp
+mkdir neovim && cd neovim
+curl -LO https://github.com/neovim/neovim/archive/refs/tags/v${NVIMVER}.tar.gz
+tar xvzf "v${NVIMVER}.tar.gz"
+cd "neovim-${NVIMVER}"
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build
+cpack -G DEB
+sudo dpkg -i --force-overwrite "nvim-linux-${ARCHTYPE}.deb"
 
 sudo cp -r "$PWD/dotfiles/nvim" "$XDG_CONFIG_HOME"/nvim
 sudo cp -r "$PWD/dotfiles/.tmux.conf" "$VSCODE_HOME"/.tmux.conf
