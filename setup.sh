458156ee3af030a960229115419654177e1ce649
Bill Hughes <whughesiii@pm.me>
2026-03-20 18:09:55 -0500

    undo more

--- setup.sh
+++ setup.sh
@@ -41,13 +41,12 @@ ln -sfn "$DOTFILES_DIR/dotfiles/.config/nvim" "$XDG_CONFIG_HOME/nvim"
 ln -sfn "$DOTFILES_DIR/dotfiles/.oh-my-zsh" "$HOME/.oh-my-zsh"
 ln -sfn "$DOTFILES_DIR/dotfiles/.tmux" "$HOME/.tmux"
 ln -sfn "$DOTFILES_DIR/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
-ln -sfn "$DOTFILES_DIR/dotfiles/.zshrc" "$HOME/.zshrc"
 
 ZSHRC="$HOME/.zshrc"
 DOTFILE_ALIASES="$DOTFILES_DIR/dotfiles/.zshrc"
 
 if [ -f "$ZSHRC" ]; then
-  echo "source $DOTFILES_DIR/dotfiles/.zshrc" >> "$HOME/.zshrc"
+  echo "source" "$DOTFILES_DIR/dotfiles/.zshrc" >> "$HOME/.zshrc"
 else
   ln -sfn "$DOTFILES_DIR/dotfiles/.zshrc" "$HOME/.zshrc"
 fi
